20347cc1e9c2a3cdc0470a3cddde1d9c02167676
Agattsu <agattsuma@gmail.com>
2023-12-29 16:41:49 -0300

    primeiro commit

---
+++ src/components/ExampleCOmponent.vue
@@ -0,0 +1,19 @@
+<template>
+
+<h1>
+  example
+</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'ExampleCompnent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
