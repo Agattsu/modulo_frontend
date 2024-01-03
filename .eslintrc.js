14724453fb9361998f5ce321dfd7fedb930c08b7
Agattsu <agattsuma@gmail.com>
2024-01-03 15:57:21 -0300

    add route and file to sign in

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
   // This option interrupts the configuration hierarchy at this file
@@ -6,14 +5,14 @@ module.exports = {
   root: true,
 
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: "@babel/eslint-parser",
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
+    sourceType: "module", // Allows for the use of imports
   },
 
   env: {
     browser: true,
-    'vue/setup-compiler-macros': true
+    "vue/setup-compiler-macros": true,
   },
 
   // Rules order is important, please avoid shuffling them
@@ -24,54 +23,51 @@ module.exports = {
     // Uncomment any of the lines below to choose desired strictness,
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules (look for Vuejs 2 ones)
-    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
+    "plugin:vue/essential", // Priority A: Essential (Error Prevention)
     // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
     // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
-    'airbnb-base'
-    
+    "airbnb-base",
   ],
 
   plugins: [
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
     // required to lint *.vue files
-    'vue',
-
-    
+    "vue",
   ],
 
   globals: {
-    ga: 'readonly', // Google Analytics
-    cordova: 'readonly',
-    __statics: 'readonly',
-    process: 'readonly',
-    Capacitor: 'readonly',
-    chrome: 'readonly'
+    ga: "readonly", // Google Analytics
+    cordova: "readonly",
+    __statics: "readonly",
+    process: "readonly",
+    Capacitor: "readonly",
+    chrome: "readonly",
   },
 
   // add your custom rules here
   rules: {
-    
-    'no-param-reassign': 'off',
-    'no-void': 'off',
-    'no-nested-ternary': 'off',
-    'max-classes-per-file': 'off',
+    "no-param-reassign": "off",
+    "no-void": "off",
+    "no-nested-ternary": "off",
+    "max-classes-per-file": "off",
+    'linebreak-style': 'off',
 
-    'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/prefer-default-export': 'off',
+    "import/first": "off",
+    "import/named": "error",
+    "import/namespace": "error",
+    "import/default": "error",
+    "import/export": "error",
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/prefer-default-export": "off",
     
-    'prefer-promise-reject-errors': 'off',
+    "prefer-promise-reject-errors": "off",
 
-    'vue/multi-word-component-names': 'off',
+    "vue/multi-word-component-names": "off",
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+};
